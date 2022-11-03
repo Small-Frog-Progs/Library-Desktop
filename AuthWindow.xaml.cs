d4394d38e0a8dcea092233f93a9e264f1c851950
OFFicerJO <egorik2509@gmail.com>
2022-11-03 17:11:14 +0700

    new commit

--- AuthWindow.xaml.cs
+++ AuthWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Library
@@ -17,11 +19,18 @@ namespace Library
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
+
+            AuthFrame.Navigate(new AuthPage());
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
