359ac9bcd1d6114aa85855d7ebaf3984c2b18db8
Uotan Heinz <61697058+Uotan@users.noreply.github.com>
2022-11-04 01:21:03 +0700

    new commit

--- Controls/AuthorController.cs
+++ Controls/AuthorController.cs
@@ -1,26 +1,23 @@
-﻿using System;
+﻿using Library.Common;
+using Library.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Library.Common;
-using Library.Models;
-using Newtonsoft.Json;
 
 namespace Library.Controls
 {
-    public class Authorization
+    public class AuthorController
     {
-        /// <summary>
-        /// Метод возвращает токен авторизации либо "error"
-        /// </summary>
-        /// <param name="_email">Почта</param>
-        /// <param name="_pass">Пароль</param>
-        /// <returns></returns>
-        public string SignIn(string _email, string _pass)
+        public List<Author> GetAuthors()
         {
+
+            List<Author> _authorList = new List<Author>();
+
             WebClient client = new WebClient();
             NameValueCollection param = new NameValueCollection();
             param.Add("email", _email);
@@ -31,9 +28,7 @@ namespace Library.Controls
                 var response = client.UploadValues(ServerAddress.SrvrAddres + "api/login", "POST", param);
                 string result = Encoding.Default.GetString(response);
                 result = result.Trim();
-                BearerClass userData = JsonConvert.DeserializeObject<BearerClass>(result);
-                return userData.Bearer;
-                //return result;
+                return result;
             }
             catch (Exception)
             {
@@ -41,31 +36,37 @@ namespace Library.Controls
             }
         }
 
-
-        /// <summary>
-        /// Не активный метод регистрации
-        /// </summary>
-        /// <param name="_email"></param>
-        /// <param name="_pass"></param>
-        /// <returns></returns>
-        public string SignUn(string _email, string _pass)
+        public object AddAuthor(string _nameAuthor)
         {
-            return null;
-
-
             WebClient client = new WebClient();
             NameValueCollection param = new NameValueCollection();
-            param.Add("email", _email);
-            param.Add("password", _pass);
+            param.Add("nameAuthor", _nameAuthor);
             try
             {
 
                 var response = client.UploadValues(ServerAddress.SrvrAddres + "api/login", "POST", param);
                 string result = Encoding.Default.GetString(response);
                 result = result.Trim();
-                BearerClass userData = JsonConvert.DeserializeObject<BearerClass>(result);
-                return userData.Bearer;
-                //return result;
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public string DeleteAuthor(string _id)
+        {
+            WebClient client = new WebClient();
+            NameValueCollection param = new NameValueCollection();
+            param.Add("id", _id);
+            try
+            {
+
+                var response = client.UploadValues(ServerAddress.SrvrAddres + "api/login", "POST", param);
+                string result = Encoding.Default.GetString(response);
+                result = result.Trim();
+                return result;
             }
             catch (Exception)
             {
