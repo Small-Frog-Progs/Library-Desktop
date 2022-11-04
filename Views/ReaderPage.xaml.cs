6d4bd6022ba9581eb33b9c5105e7409a3aa4e393
OFFicerJO <egorik2509@gmail.com>
2022-11-04 14:44:50 +0700

    Readers+Controller

--- Views/ReaderPage.xaml.cs
+++ Views/ReaderPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Library.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,29 +22,70 @@ namespace Library.Views
     /// </summary>
     public partial class ReaderPage : Page
     {
+        Controls.UserController controller = new Controls.UserController();
+        List<User> users = new List<User>();
+        IEnumerable<User> users1 = new List<User>();
+
         public ReaderPage()
         {
             InitializeComponent();
+
+            UpdateData();
+        }
+        private void UpdateData()
+        {
+            users = controller.GetUsers();
+            listReaders.ItemsSource = users;
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                controller.DeleteUser(((User)listReaders.SelectedItem).id.ToString());
+                UpdateData();
+            }
+            catch (Exception)
+            {
+            }
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                DateTime? selectedDate = dpDateOfBirth.SelectedDate;
 
+                controller.CreateUser(tbName.Text, tbEmail.Text, selectedDate.Value.Date.ToShortDateString(), "123");
+                UpdateData();
+            }
+            catch (Exception)
+            {
+            }
         }
 
         private void tbNameSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            SearchUser();
         }
 
         private void tbIdSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
+            SearchUser();
+        }
+        private void SearchUser()
+        {
+            try
+            {
+                users1 = users;
+                users1 = users.Where(x => x.id.ToString().Contains(tbIdSearch.Text));
+                users1 = users.Where(x => x.name.ToLower().Contains(tbNameSearch.Text.ToLower()));
+                listReaders.ItemsSource = users1;
+            }
+            catch (Exception)
+            {
 
+            }
         }
     }
 }
